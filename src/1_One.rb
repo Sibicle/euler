87f99a4d55a4eefeb5dbab4439686ed7084c7fb0
Jason N <me@sibicle.com>
2020-03-25 15:58:14 -0600

    solve problem 1

--- src/1_One.rb
+++ src/1_One.rb
@@ -1,5 +1,12 @@
 class One
   def self.run
-    print "problem 1"
+    total = 0
+    for i in 0..999 do
+      if i % 3 == 0 || i % 5 == 0
+        total += i
+      end
+    end
+
+    puts total
   end
 end
