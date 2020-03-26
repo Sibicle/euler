bc62a737dfab4e0ffad1f018653711f2ce70e4ed
Jason N <me@sibicle.com>
2020-03-26 01:09:24 -0600

    add second (slower) solution

--- src/1_One.rb
+++ src/1_One.rb
@@ -9,4 +9,20 @@ class One
 
     puts total
   end
+
+  def self.run_two
+    numbers = []
+    threes = fives = 0
+    while fives < 1000 do
+      numbers.push fives
+      fives += 5
+    end
+
+    while threes < 1000 do
+      numbers.push threes unless numbers.include? threes
+      threes += 3
+    end
+
+    # puts numbers.sum
+  end
 end
