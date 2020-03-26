7035b95b1581acafd3181aa7266e83188b8f4d2a
Jason N <me@sibicle.com>
2020-03-26 01:19:55 -0600

    solution 2

--- src/2_Two.rb
+++ src/2_Two.rb
@@ -1,5 +1,16 @@
 class Two
   def self.run
-    print "problem 2"
+    n0 = 1
+    n = total = 2
+
+    while n < 4000000 do
+      temp = n
+      n += n0
+      n0 = temp
+
+      total += n if n % 2 == 0
+    end
+
+    puts total
   end
 end
